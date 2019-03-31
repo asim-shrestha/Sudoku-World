187c0cabaa2f10bec488115bb7728c73841cf44f
adam-watkins <cadamrun@gmail.com>
2019-03-31 16:42:36 -0700

    Add pair input validation

--- app/src/main/java/com/sigma/sudokuworld/masterdetail/detail/AddPairActivity.java
+++ app/src/main/java/com/sigma/sudokuworld/masterdetail/detail/AddPairActivity.java
@@ -1,6 +1,7 @@
 package com.sigma.sudokuworld.masterdetail.detail;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import com.sigma.sudokuworld.R;
 import com.sigma.sudokuworld.persistence.db.entities.Language;
@@ -33,16 +34,40 @@ public class AddPairActivity extends AbstractDrillDownActivity {
         Language nLang = mAddPairFragment.getNativeLanguage();
         Language fLang = mAddPairFragment.getForeignLanguage();
 
+        //Input validation
+        boolean isValidWordPair = true;
+        String errorMsg = "";
 
-        if (nWord != null && fWord != null) {
-            if (!nWord.isEmpty() && !fWord.isEmpty()) {
-
-                Word nativeWord = new Word(0, nLang.getLanguageID(), nWord);
-                Word foreignWord = new Word(0, fLang.getLanguageID(), fWord);
-
-                mMasterDetailViewModel.saveWordPair(nativeWord, foreignWord);
-                finish();
-            }
+        if (nWord == null || fWord == null) {
+            errorMsg = "Words cannot be null";
+            isValidWordPair = false;
         }
+
+        else if (nLang == null || fLang == null) {
+            errorMsg = "Words cannot be null";
+            isValidWordPair = false;
+        }
+
+        else if (nWord.isEmpty()) {
+            errorMsg = "Native word cannot be left blank";
+            isValidWordPair = false;
+        }
+
+        else if (fWord.isEmpty()) {
+            errorMsg = "Foreign word cannot be left blank";
+            isValidWordPair = false;
+        }
+
+
+        if (isValidWordPair) {
+            Word nativeWord = new Word(0, nLang.getLanguageID(), nWord);
+            Word foreignWord = new Word(0, fLang.getLanguageID(), fWord);
+
+            mMasterDetailViewModel.saveWordPair(nativeWord, foreignWord);
+            finish();
+        } else {
+            Snackbar.make(mCoordinatorLayout, errorMsg, Snackbar.LENGTH_SHORT).show();
+        }
+
     }
 }
