8a24c9cf3f51d640644b5e7e3ee14397508c2ae9
Asim Shrestha <ashresth@sfu.ca>
2019-03-31 12:34:52 -0700

    Toasts displaying properly for cells

--- app/src/main/java/com/sigma/sudokuworld/sudoku/singleplayer/LongTouchHandler.java
+++ app/src/main/java/com/sigma/sudokuworld/sudoku/singleplayer/LongTouchHandler.java
@@ -1,5 +1,6 @@
 package com.sigma.sudokuworld.sudoku.singleplayer;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Looper;
 import android.speech.tts.TextToSpeech;
@@ -21,68 +22,88 @@ public class LongTouchHandler {
     private TextToSpeech mTTS;
 
     private Timer mTimer;
-    private TimerTask mGridLongClickTimer;
+    private TimerTask mGridLongClickTask;
 
     private int mCellTouched;
 
     public LongTouchHandler(Context context, GameViewModel gameViewModel) {
         mContext = context;
         mGameViewModel = gameViewModel;
-
-        //Initializing TTS
-        mTTS = new TextToSpeech(mContext, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS){
-                    //Set Lang
-                    if (mGameViewModel.getGameMode() == GameMode.NATIVE) {
-                        mTTS.setLanguage(new Locale("fr"));
-                    } else {
-                        mTTS.setLanguage(new Locale("en"));
-                    }
-                }
-            }
-        });
+        initTTS();
+        initLooper();
     }
 
     private boolean isComprehensionMode() {
         return PersistenceService.loadAudioModeSetting(mContext);
     }
 
-    private void resetGridLongClickTimer(){
-        mGridLongClickTimer = new TimerTask() {
-            @Override
-            public void run() {
-                mTimer.purge();
-                if (mGameViewModel.isLockedCell(mCellTouched)) {
-                    String text = mGameViewModel.getMappedString(
-                            mGameViewModel.getCellValue(mCellTouched),
-                            GameMode.opposite(mGameViewModel.getGameMode())
-                    );
-                    if(isComprehensionMode())
-                    {
-                        mTTS.speak(text,TextToSpeech.QUEUE_FLUSH,null,null);
-                    }
-                    else
-                    {
-                        prepareLooper();
-                        Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
+    private void initTTS(){
+        if (mTTS == null) {
+            mTTS = new TextToSpeech(mContext, new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    if (status == TextToSpeech.SUCCESS) {
+                        //Set Lang
+                        if (mGameViewModel.getGameMode() == GameMode.NATIVE) {
+                            mTTS.setLanguage(new Locale("fr"));
+                        } else {
+                            mTTS.setLanguage(new Locale("en"));
+                        }
                     }
                 }
-            }
-        };
+            });
+        }
     }
-    private void prepareLooper(){
+
+    private void initLooper(){
         //Create a looper if there is none
-        if(Looper.myLooper() == null){ Looper.prepare(); }
+        if( Looper.myLooper() == null){ Looper.prepare(); }
     }
+
     public void handleGridLongClick(int cellTouched){
         mCellTouched = cellTouched;
         if(mTimer == null){
             //Create a new timer to be run
             mTimer = new Timer();
             resetGridLongClickTimer();
-            mTimer.schedule(mGridLongClickTimer, 650);
+            mTimer.schedule(mGridLongClickTask, 650);
+        }
+    }
+    
+    private void resetGridLongClickTimer(){
+        mGridLongClickTask = new TimerTask() {
+            @Override
+            public void run() {
+                //Executes when timer finishes
+                mTimer.purge();
+                Activity mainActivity = (Activity) mContext;
+
+                //Run on ui thread to properly display toasts
+                mainActivity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        gridLongClick();
+
+                    }
+                });
+
+            }
+        };
+    }
+
+    private void gridLongClick(){
+        if (mGameViewModel.isLockedCell(mCellTouched)) {
+            String text = mGameViewModel.getMappedString(
+                    mGameViewModel.getCellValue(mCellTouched),
+                    GameMode.opposite(mGameViewModel.getGameMode()));
+
+            if (isComprehensionMode()) {
+                initTTS();
+                mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+            } else {
+                initLooper();
+                Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -101,11 +122,12 @@ public class LongTouchHandler {
                 GameMode.opposite(mGameViewModel.getGameMode()));
 
         if (isComprehensionMode()){
+            initTTS();
             mTTS.speak(buttonText,TextToSpeech.QUEUE_FLUSH,null,null);
         }
 
         else {
-            prepareLooper();
+            initLooper();
             Toast.makeText(mContext, buttonText, Toast.LENGTH_SHORT).show();
         }
         return true;
